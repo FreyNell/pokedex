f074f178403e7e7289392d0524b7813182458aeb
FreyNell <freybalaguera@gmail.com>
2020-08-19 13:29:34 -0500

    Let's do it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+/**
+ *  selector: es donde se pinta el elemento.
+ *  templateUrl: la plantilla html que utilizará el elemento.
+ *  styleUrls: estilos que utilizará el elemento.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Las vairables que puedo usar en el template html
 export class AppComponent {
   title = 'pokedex';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 @NgModule({
+  // Componentes van aquí
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent
   ],
+  // Módulos van aquí
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // Servicios van aquí
   providers: [],
+  // Componente raíz va aquí
   bootstrap: [AppComponent]
 })
 export class AppModule { }
